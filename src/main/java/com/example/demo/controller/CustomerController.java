3a117ba7b492dc940b70557ab32856a6e403b822
raminta.bingelyte <raminta.bingelyte@bigbank.lt>
2022-11-30 21:42:02 +0200

    Add CustomerService.javacustomerService: getAllCustomers getCustomer

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,17 +1,42 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Customer;
+import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/customer")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
 
     // http://127.0.0.1:8080/customer/test
     @GetMapping(path = "/test")
     public @ResponseBody String getTestString(){
         return "Hello";
     }
+
+    // http://127.0.0.1:8080/customer/all
+    @GetMapping(path = "/all")
+    public @ResponseBody
+    Iterable<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    // http://127.0.0.1:8080/customer/114
+    @GetMapping(path = "/{id}")
+    public @ResponseBody
+    Optional<Customer> getCustomer(@PathVariable int id){
+        return customerService.getCustomer(id);
+    }
+
+
 }
--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,7 +10,7 @@ import java.util.Optional;
 @Service
 
 public class CustomerService {
-
+@Autowired
     private CustomerRepository customerRepository;
 
     public Iterable<Customer> getAllCustomers(){
