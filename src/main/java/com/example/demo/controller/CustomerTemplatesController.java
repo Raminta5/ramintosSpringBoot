571286af7bf78beb6ab020aee37702a33fc5a59a
raminta.bingelyte <raminta.bingelyte@bigbank.lt>
2022-12-05 21:22:31 +0200

    Add CustomerController, html file

--- src/main/java/com/example/demo/controller/CustomerTemplatesController.java
+++ src/main/java/com/example/demo/controller/CustomerTemplatesController.java
@@ -1,6 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/customer/templates")
 public class CustomerTemplatesController {
+
+
+        @Autowired
+        private CustomerService customerService;
+
+    // http://127.0.0.1:8080/customer/templates/test
+    @GetMapping(path = "/test")
+    public String getTestString() {
+        return "html_test";
+    }
 
 
 }
