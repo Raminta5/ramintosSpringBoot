181e6a211ea9c4ebe15aed5216db229b1556d921
raminta.bingelyte <raminta.bingelyte@bigbank.lt>
2022-12-05 21:40:10 +0200

    Add CustomerTemplatesController

--- src/main/java/com/example/demo/controller/CustomerTemplatesController.java
+++ src/main/java/com/example/demo/controller/CustomerTemplatesController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/customer/templates")
@@ -19,6 +25,17 @@ public class CustomerTemplatesController {
     @GetMapping(path = "/test")
     public String getTestString() {
         return "html_test";
+    }
+
+    //  http://localhost:8080/customer/templates/firstpage/112
+    @GetMapping(path = "/firstpage/{id}")
+    public String getCustomerDetails(@PathVariable int id, Model model) {
+        Optional<Customer> customer = customerService.getCustomerById(id);
+        model.addAttribute("company_name", customer.get().getCustomerName());
+        model.addAttribute("contact_last_name", customer.get().getContactLastName());
+        model.addAttribute("contact_first_name", customer.get().getContactFirstName());
+        model.addAttribute("country", customer.get().getCountry());
+        return "firstpage";
     }
 
 
