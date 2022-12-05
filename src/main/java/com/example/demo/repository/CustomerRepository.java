0f08e0b369dc070b407134a0ca092c5be50fff87
raminta.bingelyte <raminta.bingelyte@bigbank.lt>
2022-12-05 20:27:57 +0200

    Add CustomerRepository

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -3,8 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-
-
+    // SELECt * FROM customers WHERE customerName like 'a%'
+    Optional<Customer> findByCustomerNumber(int id);
+    Optional<Customer> findByCustomerName(String name);
+    Iterable<Customer> findByCustomerNameLike(String name); // '%s'; '%new%'
 }
